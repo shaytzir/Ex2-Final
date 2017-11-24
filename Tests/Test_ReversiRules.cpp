73737957cbe70c500f5f13ccad20c767d351c91b
shaytzir <shay@tzirin.com>
2017-11-24 06:30:24 +0200

    start of unit tests

--- Tests/Test_ReversiRules.cpp
+++ Tests/Test_ReversiRules.cpp
@@ -2,3 +2,24 @@
 // Created by shay on 11/24/17.
 //
 
+#include <gtest/gtest.h>
+#include "../src/ReversiRules.h"
+#include "../src/HumanP.h"
+#include "../src/Console.h"
+
+
+namespace {
+    class Test_ReversiRules: public testing::Test {
+
+    public:
+        ReversiRules rules = ReversiRules(new HumanP('X'), new HumanP('O'), new Console());
+
+        Test_ReversiRules() {
+            rules;
+        }
+    };
+}
+
+TEST_F(Test_ReversiRules, isGameOver) {
+    EXPECT_FALSE(rules.gameover());
+}
--- gtest_main.cpp
+++ gtest_main.cpp
@@ -5,7 +5,8 @@
 #include <gtest/gtest-typed-test.h>
 #include <gtest/gtest.h>
 
-GTEST_API_ int main(int argc, char **argv) {
+
+GTEST_API_ int gtest_main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
