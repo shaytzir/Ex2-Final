70dd325842696912193c002e1a57e41bb1f9566c
shaytzir <33806682+shaytzir@users.noreply.github.com>
2017-11-28 21:43:00 +0200

    Delete CTestTestfile.cmake

--- cmake-build-debug/lib/googletest-master/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/shay/CLionProjects/Ex2-shay/lib/googletest-master
-# Build directory: /home/shay/CLionProjects/Ex2-shay/cmake-build-debug/lib/googletest-master
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
